680c78b63a8dc95fbdaa8368cc8d37ee0dbcd730
Sergey Margelov <smargelov@gmail.com>
2021-04-24 23:21:17 +0300

    Styling QuizCreator component

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import classes from "./QuizCreator.module.sass";
+import Input from "../../components/Ui/Input/Input";
+import Button from "../../components/Ui/Button/Button";
 
 class QuizCreator extends React.Component {
+    submitHandler = event => {
+        event.preventDefault()
+    }
+    addQuestionHandler = () => {
+
+    }
+
+    createQuizHandler = () => {
+
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz Creator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Quiz Creator</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <Input
+                        label="Ввести вопрос"
+                        />
+                        <hr/>
+                        <Input/>
+                        <Input/>
+                        <Input/>
+                        <Input/>
+
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}>
+                            Добавать вопрос
+                        </Button>
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}>
+                            Создать тест
+                        </Button>
+
+                    </form>
+                </div>
             </div>
         )
     }
